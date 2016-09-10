e3c1e6fc07f9c4e243002e631195cebbf7f9e893
adi <adityaa803@gmail.com>
2016-09-10 22:10:15 +0530

    'pushed_by_py'

--- first.py
+++ first.py
@@ -2,12 +2,13 @@ import subprocess
 # "git commit -m 'Hellopython'",
 cmds = [
     "git add .",
-    "git commit -m 'Hellopython'",
+    "git commit -m ",
     "git status"
 ]
+commitMessage = "'" + raw_input().strip() + "'"
+cmds[1] += "_".join(commitMessage.split())
 for cmd in cmds:
     query = cmd.split()
-    print query
     process = subprocess.Popen(query, stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
