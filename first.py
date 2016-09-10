4f47a04df308a2b6ffbfea965dad379537a6f72f
adi <adityaa803@gmail.com>
2016-09-10 22:11:06 +0530

    'a_x_x'

--- first.py
+++ first.py
@@ -8,6 +8,7 @@ cmds = [
 commitMessage = "'" + raw_input().strip() + "'"
 cmds[1] += "_".join(commitMessage.split())
 for cmd in cmds:
+    print cmd
     query = cmd.split()
     process = subprocess.Popen(query, stdout=subprocess.PIPE)
     output = process.communicate()[0]
