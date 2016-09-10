93ebaab6ec9a9cc9401b7af13c8f67244ea36337
adi <adityaa803@gmail.com>
2016-09-10 22:32:09 +0530

    'a_x_b'

--- first.py
+++ first.py
@@ -11,6 +11,13 @@ cmds[1] += "_".join(commitMessage.split())
 for cmd in cmds:
     print ">>", cmd
     query = cmd.split()
-    process = subprocess.Popen(query, stdout=subprocess.PIPE)
+    # process = subprocess.Popen(query, stdout=subprocess.PIPE)
+    if(query[1] == "push"):
+        process = subprocess.Popen(
+            query, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+        process.communicate("itaditya")
+        process.communicate("9911502984.Ad")
+    else:
+        process = subprocess.Popen(query, stdout=subprocess.PIPE)
     output = process.communicate()[0]
     print output
