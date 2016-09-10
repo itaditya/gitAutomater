9b8d37e6be01172bd1bbe5d79534a15b4cb6fff2
adi <adityaa803@gmail.com>
2016-09-10 23:09:11 +0530

    'bleeding_edge'

--- first.py
+++ first.py
@@ -1,27 +1,31 @@
 import subprocess
 # "git commit -m 'Hellopython'",
+process = subprocess.Popen(
+    "git config credential.helper store", stdout=subprocess.PIPE, shell=True)
+
 cmds = [
     "git add .",
     "git commit -m ",
     "git status",
     "git push origin master",
 ]
+print "Enter a commit message or $ for manual git"
 commitMessage = "'" + raw_input().strip() + "'"
-cmds[1] += "_".join(commitMessage.split())
-for cmd in cmds:
-    print ">>", cmd
-    query = cmd.split()
-    # process = subprocess.Popen(query, stdout=subprocess.PIPE)
-    process = 0
-    if(query[1] == "push"):
-        print "push"
-        process = subprocess.Popen(
-            query, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
-        # process.stdin.write("itaditya")
-        # process.stdin.write("9911502984.Ad")
-    else:
-        process = subprocess.Popen(query, stdout=subprocess.PIPE)
-    output = process.communicate()[0]
-    print output
+if commitMessage == "'$'":
+    print "Manual Git selected"
+else:
+    cmds[1] += "_".join(commitMessage.split())
+    for cmd in cmds:
+        query = cmd.split()
+        # process = subprocess.Popen(query, stdout=subprocess.PIPE)
+        if(query[1] == "push"):
+            process = subprocess.Popen(
+                query, stdout=subprocess.PIPE, stdin=subprocess.PIPE)
+            # process.stdin.write("itaditya")
+            # process.stdin.write("9911502984.Ad")
+        else:
+            process = subprocess.Popen(query, stdout=subprocess.PIPE)
+        output = process.communicate()[0]
+        print output
 
 print 'All done!'
